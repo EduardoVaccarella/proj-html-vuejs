8cbb54e041e5f337ab5ed0ea091e2cc0860c784b
eduardoego <eduardo0vaccarella@gmail.com>
2022-03-24 11:31:40 +0100

    quasi finita la sezione con le card

--- src/components/CoursesProj.vue
+++ src/components/CoursesProj.vue
@@ -1,7 +1,48 @@
 <template>
   <div class="container-fluid">
-    <div class="ms-container">
+    <div class="ms-container d-flex justify-content-center">
+        <h1>
+            new courses
+        </h1>
 
+        <div class="card-wrapper d-flex justify-content-center">
+            <div class="card" v-for="(card,i) in cards" :key="i">
+                <div class="img-wrapper">
+                    <img :src="card.img" alt="img">
+                </div>
+                <div class="content-wrapper">
+                    <h6>
+                        {{ card.text }}
+                    </h6>
+                    <p>
+                        {{ card.category }}
+                    </p>
+                    <div class="line">
+
+                    </div>
+                    <div class="icon-wrapper">
+                        <div>
+                            <font-awesome-icon icon="fa-solid fa-signal" />
+                            <span>
+                                Advanced
+                            </span>
+                        </div>
+                        <div>
+                            <font-awesome-icon icon="fa-solid fa-bars" />
+                            <span>
+                                8 Lectures
+                            </span>
+                        </div>
+                        <div>
+                            <font-awesome-icon icon="fa-solid fa-clock" />
+                            <span>
+                                6 hours
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -11,7 +52,38 @@ export default {
   name: 'CoursesProj',
   data() {
     return {
-
+            cards: [
+                {
+                    text: 'How to be a DJ? Make Electronic Music',
+                    category: 'Electronic',
+                    img: '../assets/img/12345-1-370x200.png'
+                },
+                {
+                    text: 'Nvidia and UE4 Technologies Practice',
+                    category: 'Nvidia',
+                    img: '../assets/img/12345-1-370x200.png'
+                },
+                {
+                    text: 'Fashion Photography from professional',
+                    category: 'Fashion',
+                    img: '../assets/img/12345-1-370x200.png'
+                },
+                {
+                    text: 'Design Instrument for Communication',
+                    category: 'Communication',
+                    img: '../assets/img/12345-1-370x200.png'
+                },
+                {
+                    text: 'Make your Concept Right and Beautiful',
+                    category: 'Art',
+                    img: '../assets/img/12345-1-370x200.png'
+                },
+                {
+                    text: 'Read Bike Manual or How to be a Champion',
+                    category: 'Bicycling',
+                    img: '../assets/img/12345-1-370x200.png'
+                }
+            ]
         }
     }
 }
@@ -25,6 +97,77 @@ export default {
 }
 
 .ms-container {
-    height: 894px
+    height: 894px;
+    padding-top: 60px;
+    padding-bottom: 64px;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+        font-size: 23px;
+        font-weight: bold;
+        text-transform: capitalize;
+        margin-bottom: 45px;
+        text-align: center;
+    }
+
+    .card-wrapper {
+        flex-wrap: wrap;
+        gap: 19px;
+
+        .card {
+            height: 303px;
+            width: 251px;
+        }
+
+        .img-wrapper {
+            width: 251px;
+            height: 167px;
+            background-position: center;
+            background-size: cover;
+
+            img {
+                width: 250px;
+                height: 167px;
+            }
+        }
+    }
+
+    .content-wrapper {
+        text-align: center;
+        padding: 14px;
+        padding-top: 21px;
+        padding-bottom: 10px;
+
+        h6 {
+            font-size: 14px;
+            font-weight: 600;
+            margin-bottom: 12px;
+        }
+
+        p {
+            font-size: 12px;
+            color: $silver;
+            margin-bottom: 15px;
+        }
+
+        .line {
+            border-bottom: 1px solid $silver;
+            width: 100%;
+            margin-bottom: 8px;
+        }
+
+        .icon-wrapper {
+            display: flex;
+            justify-content: space-between;
+            font-size: 10px;
+            color: $buttercup;
+
+            span {
+                color: $mineShaft;
+                font-size: 8px;
+            }
+        }
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -17,11 +17,15 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
+import { faSignal } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* add icons to the library */
-library.add(faLinkedin, faInstagram, faFacebook, faTwitter)
+library.add(faLinkedin, faInstagram, faFacebook, faTwitter, faBars, faClock, faSignal)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
